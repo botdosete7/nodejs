a950fb6521e14667193ad3c1050d293d8573b530
botdosete7 <maretadosete@gmail.com>
2025-10-03 00:16:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
 
+const express = require("express");
 const app = express();
-const PORT = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => {
+  res.send("🚀 Olá, Railway rodando com Node.js!");
+});
 
-// Use the router for handling routes
-app.use('/', indexRouter);
-
-// Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
